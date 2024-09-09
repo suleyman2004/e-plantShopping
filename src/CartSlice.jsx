f92124488e1b31f199782e8f796cc6e9ab6448a7
suleyman2004 <128624857+suleyman2004@users.noreply.github.com>
2024-09-09 12:31:47 +0700

    Update CartSlice.jsx

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,19 +4,40 @@ export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
     items: [], // Initialize items as an empty array
+    totalItems: 0
   },
   reducers: {
     addItem: (state, action) => {
-    
+    const {name, image, cost} = action.payload;
+    const existingItem = state.items.find(item => item.name === name);
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      state.items.push({ name, image, cost, quantity: 1 });
+    }
+    state.totalItems++;
     },
     removeItem: (state, action) => {
+      const { name } = action.payload;
+      const ItemToRemove = state.items.find(item => item.name === name);
+
+      if (ItemToRemove) {
+        state.totalItems -= ItemToRemove.quantity; 
+        state.items = state.items.filter(item => item.name !== name);
+      }
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name, quantity} = action.payload;
+      const itemToUpdate = state.items.find(item => item.name === name);
+      if (itemToUpdate){
+        state.totalItems += (quantity - itemToUpdate.quantity);
+        itemToUpdate.quantity = quantity;
+      }
     },
   },
 });
+
+export const selectTotalItems = state => state.cart.totalItems;
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
 
